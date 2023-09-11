72da01a8fb9a891349d90f8847e99b0bfd248b4d
Zhicai <zzhang2@caltech.edu>
2023-09-10 17:53:03 -0700

    update

--- src/testINO.cpp
+++ src/testINO.cpp
@@ -14,15 +14,18 @@
 #include <HYT271.h>
 #include <ComIOException.h>
 #include <SPIFTDICom.h>
-#include <LMK03806.h>
-#include <PEBBLES.h>
+#include <LMK03806INO.h>
+#include <PEBBLESINO.h>
+#include <TextSerialCom.h>
+#include <SerialCom.h>
+#include "Logger.h"
 
 
 int nDivide = 9;
 
 int tsleep_write = 50;
 
-void configureClock(std::shared_ptr<LMK03806> clock){
+void configureClock(std::shared_ptr<LMK03806INO> clock){
   for(int reg =0; reg < 31; reg++)
   {
       clock->read(reg);
@@ -156,53 +159,22 @@ int main(int argc, char** argv)
     nDivide = std::stoi(argv[1]);
   }
 
-  std::shared_ptr<FT232H> ft232;
-    try {
-      ft232 = std::make_shared<FT232H>(MPSSEChip::Protocol::SPI0, MPSSEChip::Speed::FOUR_HUNDRED_KHZ, MPSSEChip::Endianness::MSBFirst, "","pebbles");
-      std::cout << "Initialized FT232H device: " << ft232->to_string() << std::endl;
-    } catch(std::exception& e) {
-      std::cout << "Failed to initialize FT232H device: " << e.what() << std::endl;
-      return 1;
-    }
+  std::shared_ptr<TextSerialCom> com(new TextSerialCom("/dev/ttyACM0", SerialCom::BaudRate::Baud9600));
+  com->setTermination("\n");
+  com->init();
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
- // clock chip connection: SCL, SDI, DSO and LATCH are connected to FT232H pins D0, D1, D2 and D4 respectively
- // pebbles connection:
- // GPIO pins: 0 <-> D4, 1 <-> D5, 2 <-> D6, 3 <-> D7, 4 <-> C0, 5 <-> C1, 6 <-> C2, 7 <-> C3, 8 <-> C4, 9 <-> C5, 10 <-> C6, 11 <-> C7
-
-
-  std::shared_ptr<SPIFTDICom> com(new SPIFTDICom(ft232));
-
-  std::shared_ptr<LMK03806> clock(new LMK03806(com, ft232));
+  //clock
+  std::shared_ptr<LMK03806INO> clock(new LMK03806INO(com));
   configureClock(clock);
 
-  std::shared_ptr<PEBBLES> pebbles(new PEBBLES(com, ft232, 10));
-
-  std::cout<<"make clock on pin 1 and data on pin 2"<<std::endl;
-  for(int i=0; i<1000; i++){
-
-  ft232->gpio_write(2, 1);
-  ft232->gpio_write(1, 1);
-  ft232->gpio_write(1, 0);
-  ft232->gpio_write(2, 0);
-  ft232->gpio_write(1, 1);
-  ft232->gpio_write(1, 0);
-
+  std::shared_ptr<PEBBLESINO> pebbles(new PEBBLESINO(com));
+  for(int i=0; i<10; i++) {
+  pebbles->writeGPIO("MODE", 0);
+  std::this_thread::sleep_for(std::chrono::milliseconds(1));
+  pebbles->writeGPIO("MODE", 1);
+  std::this_thread::sleep_for(std::chrono::milliseconds(1));
   }
-  //std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  //ft232->read(128);
-  /*
-  pebbles->read(0);
-  std::this_thread::sleep_for(std::chrono::milliseconds(100));
-  pebbles->read(0);
-  std::this_thread::sleep_for(std::chrono::milliseconds(100));
-  pebbles->read(0);
-  std::this_thread::sleep_for(std::chrono::milliseconds(100));
-  pebbles->read(0);
-  std::this_thread::sleep_for(std::chrono::milliseconds(100));
-  pebbles->read(0);
-  std::this_thread::sleep_for(std::chrono::milliseconds(100));
-  pebbles->read(0);
-  */
 
 
   return 0;
